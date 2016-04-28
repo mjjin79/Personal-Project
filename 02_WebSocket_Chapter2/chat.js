feb906be4fe5d6435ebc4ed7626c2765f9e35ec4
raphael <raphael@tmon.co.kr>
2016-04-28 17:42:46 +0900

    2016.04.28 - websocket chapter2

---
+++ 02_WebSocket_Chapter2/chat.js
@@ -0,0 +1,44 @@
+window.onload = function() {
+    var textView = document.getElementById("text-view");
+    var buttonSend = document.getElementById("send-button");
+    var buttonStop = document.getElementById("stop-button");
+    var label = document.getElementById("status-label");
+    
+    var socket = new WebSocket("ws://echo.websocket.org");
+    
+    socket.onopen = function(event) {
+        label.innerHTML = "Connection open";
+    }
+    
+    socket.onmessage = function(event) {
+        if (typeof event.data === "string") {
+            label.innerHTML = label.innerHTML + "<br />" + event.data;
+        }
+    }
+    socket.onclose = function(event) {
+        var code = event.code;
+        var reason = event.reason;
+        var wasClean = event.wasClean;
+        
+        if (wasClean) {
+            label.innerHTML = "Connection closed normally.";
+        }else{
+            label.innerHTML = "Connection closed with message: " + reason + " (Code: " +code + ")";
+        }
+    }
+    
+    socket.onerror = function(event) {
+        label.innerHTML = "Error: " + event;
+    }
+    
+    buttonSend.onclick = function() {
+        if (socket.readyState == WebSocket.OPEN) {
+            socket.send(textView.value);
+        }
+    }
+    buttonStop.onclick = function() {
+        if (socket.readyState == WebSocket.OPEN) {
+            socket.close();
+        }
+    }
+}
